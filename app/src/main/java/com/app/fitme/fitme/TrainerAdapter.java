8479b481fc73d5699bd65324873da28bf829b389
sahar <saharmelechi22@gmail.com>
2018-11-14 19:31:20 +0200

    Sahar - Implement get view adapter by TrainerView.

--- app/src/main/java/com/app/fitme/fitme/TrainerAdapter.java
+++ app/src/main/java/com/app/fitme/fitme/TrainerAdapter.java
@@ -1,33 +1,42 @@
 package com.app.fitme.fitme;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TrainerAdapter extends BaseAdapter {
+    List<Trainer> trainers;
 
+    public TrainerAdapter(List<Trainer> trainers) {
+        this.trainers = trainers;
+    }
 
     private class TrainerView{
-        public String Name;
+        public TextView Name;
 
-        public String Age;
+        public TextView Age;
 
         public ImageView ImgProfile;
 
         public ImageView ImgTargil;
 
-        public TrainerView(String name, String age, ImageView imgProfile, ImageView imgTargil) {
-            Name = name;
-            Age = age;
-            ImgProfile = imgProfile;
-            ImgTargil = imgTargil;
+        public TrainerView(View name, View age, View imgProfile, View imgTargil) {
+            Name = (TextView) name;
+            Age = (TextView) age;
+            ImgProfile = (ImageView) imgProfile;
+            ImgTargil = (ImageView) imgTargil;
         }
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return this.trainers.size();
     }
 
     @Override
@@ -44,8 +53,20 @@ public class TrainerAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         if(convertView == null){
+            LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+
+            convertView = layoutInflater.inflate(R.layout.accomplishments,parent, false);
+
+            TrainerView trainer = new TrainerView( convertView.findViewById(R.id.txtName),
+                                                    convertView.findViewById(R.id.txtAge),
+                                                    convertView.findViewById(R.id.PersonPicture),
+                                                    convertView.findViewById(R.id.ImgTargil));
+                convertView.setTag(trainer);
 
         }
-        return null;
+        //Need to Implement here get tag
+        
+
+        return convertView;
     }
 }
