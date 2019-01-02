6d16f42fda86f4ac41f3162b9abfb2526b905034
sahar <saharmelechi22@gmail.com>
2019-01-02 16:59:56 +0200

    Sahar-
    changed the login to normal and gmail using fireBase.

--- app/src/main/java/com/app/fitme/fitme/AchievementsActivity.java
+++ app/src/main/java/com/app/fitme/fitme/AchievementsActivity.java
@@ -15,19 +15,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ListView;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AchievementsActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 4566;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        //TODO: Copy the log in from the chatActivity from the example projects, throw away the login activity
+
+
         //need to implement create new exercisers and call the trainer adapter
         Bitmap avatar = BitmapFactory.decodeResource(getResources(), R.drawable.default_avatar);
         Bitmap exercise = BitmapFactory.decodeResource(getResources(), R.drawable.pullup);
+
 
         Exerciser defaultExercise = new Exerciser("default", 22f, avatar, exercise);
 
@@ -55,16 +65,14 @@ public class AchievementsActivity extends AppCompatActivity {
             }
         });
 
-        FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-                if (firebaseUser == null) {
-                    Intent intent = new Intent(AchievementsActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                }
-            }
-        };
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                new AuthUI.IdpConfig.EmailBuilder().build()))
+                        .build(),
+                RC_SIGN_IN);
     }
 
 }
